ba544b9d6967f6a290dfbda8c4db045e8eb6f41d
yhao <haoyucheng369@163.com>
2025-12-26 23:26:06 +0100

    test

--- AzureReactApp/src/main.tsx
+++ AzureReactApp/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { loadAppConfig } from "../public/config/runtimeConfig";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function bootstrap() {
+  await loadAppConfig();
+
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+bootstrap().catch((err) => {
+  console.error(err);
+  document.body.innerHTML = `<pre style="padding:16px;color:#b00020">Runtime config load failed:\n${String(
+    err
+  )}</pre>`;
+});

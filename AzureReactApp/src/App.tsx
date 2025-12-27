e4b2a4ce53f86e3cd0604f5e79a30ddafcce6638
yhao <haoyucheng369@163.com>
2025-12-27 07:48:20 +0100

    test

--- AzureReactApp/src/App.tsx
+++ AzureReactApp/src/App.tsx
@@ -44,9 +44,9 @@ function App() {
     <>
       <section>
         <section className="header">
-          <h1>Azure React App</h1>
+          <h1>Azure React App : OK 版本</h1>
           <p className="muted">
-            这是mep ： 有错误
+            OK
           </p>
         </section>
         <h2>Data from API (environment: {VITE_ENV})</h2>
